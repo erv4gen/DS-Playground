8692b118657f868648d2ae7a57f151d066d93451
vladimir.eremin <eremin.vladimir.d@gmail.com>
2017-11-15 20:42:30 -0600

    add comments

--- q1.py
+++ q1.py
@@ -4,7 +4,10 @@ import numpy as np
 
 filepath= './data/Energy Indicators.xls'
 
+#read file
 energy = pd.read_excel(filepath)
+
+#cleaning data
 energy = (energy[16:243]
     .drop(energy.columns[[0, 1]], axis=1)
     .rename(columns={'Environmental Indicators: Energy': 'Country',
@@ -13,9 +16,24 @@ energy = (energy[16:243]
                      'Unnamed: 5':'% Renewable'})
     .set_index('Country')
           )
+#clean missing values
+energy.replace('...',np.nan,inplace =True)
+#change rows
+energy['Energy Supply'] = energy['Energy Supply'] * 1000
 
+#change rows
+rows_to_Change = {"Republic of Korea": "South Korea",
+                  "United States of America": "United States",
+                  "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
+                  "China, Hong Kong Special Administrative Region": "Hong Kong"}
 
-print(energy.columns)
+energy = energy.reset_index()
+energy.replace({"Country": rows_to_Change},inplace = True)
+energy = energy.set_index('Country')
+
+#
+
+print(energy.head())
 
 
 
