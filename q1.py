15f0078343e9612bc30a5908c0202b2b23b3f27d
vladimir.eremin <eremin.vladimir.d@gmail.com>
2017-11-15 19:55:58 -0600

    normalised table, changed names

--- q1.py
+++ q1.py
@@ -5,15 +5,22 @@ import numpy as np
 filepath= './data/Energy Indicators.xls'
 
 energy = pd.read_excel(filepath)
-energy = energy[16:243]
-energy = energy.drop(energy.columns[[0, 1]], axis=1)
+energy = (energy[16:243]
+    .drop(energy.columns[[0, 1]], axis=1)
+    .rename(columns={'Environmental Indicators: Energy': 'Country',
+                     'Unnamed: 3':'Energy Supply',
+                     'Unnamed: 4' : 'Energy Supply per Capita',
+                     'Unnamed: 5':'% Renewable'})
+    .set_index('Country')
+          )
+
 
 print(energy.columns)
 
 
 
 energy.to_csv('./data/exti.csv')
-
+print("done!")
 
 def answer_one():
     return "ANSWER"
